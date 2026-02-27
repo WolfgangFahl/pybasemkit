199b156eeb3764e2ae31fd8353ee082648f19cac
Wolfgang Fahl <wf@bitplan.com>
2026-02-27 11:14:50 +0100

    reformats

--- tests/test_remotedebug.py
+++ tests/test_remotedebug.py
@@ -7,8 +7,7 @@ Created on 2025-112-29
 from argparse import Namespace
 
 from basemkit.basetest import Basetest
-
-from basemkit.remotedebug import RemoteDebugSetup, PathMappings, PathMapping
+from basemkit.remotedebug import PathMapping, PathMappings, RemoteDebugSetup
 
 
 class TestRemoteDebugSetup(Basetest):
@@ -28,10 +27,10 @@ class TestRemoteDebugSetup(Basetest):
             debugPort=5678,
             debugRemotePath="/remote/app",
             debugLocalPath="/local/app",
-            debug=True # Enable internal logging
+            debug=True,  # Enable internal logging
         )
 
-        debug_setup=RemoteDebugSetup(args=args)
+        debug_setup = RemoteDebugSetup(args=args)
         debug_setup.get_path_mappings()
         debug_setup.print_debug_info()
 
@@ -54,24 +53,20 @@ class TestRemoteDebugSetup(Basetest):
         self.assertEqual("/app/lib", pm.mappings[1].remote)
         self.assertEqual("C:\\Users\\Lib", pm.mappings[1].local)
 
-
     def test_path_mappings_mismatch(self):
         """
         Test that unequal list lengths raise an error.
         """
         remote_str = "/app/src"
-        local_str = "C:\\src,C:\\lib" # Two locals, one remote
+        local_str = "C:\\src,C:\\lib"  # Two locals, one remote
 
         with self.assertRaises(ValueError):
             PathMappings.from_args(remote_str, local_str)
-
 
     def test_tuple_conversion(self):
         """
         Test conversion to list of tuples for pydevd.
         """
-        pm = PathMappings(mappings=[
-            PathMapping(remote="/r", local="/l")
-        ])
+        pm = PathMappings(mappings=[PathMapping(remote="/r", local="/l")])
         expected = [("/r", "/l")]
         self.assertEqual(expected, pm.as_tuple_list())
